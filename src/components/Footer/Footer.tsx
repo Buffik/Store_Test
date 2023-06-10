a6324e7a4441ceff8167cc01657ba25d278c1123
Buffik <Som16@mail.ru>
2023-06-10 21:31:57 +0300

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,25 @@
+import Logo from 'src/UI/Logo/Logo';
 import styles from './Footer.module.scss';
+import { Link } from 'react-router-dom';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p className={styles.linksContainer}>
+          {'Developed by '}
+          <a className={styles.footer__link} href="https://github.com/Buffik">
+            Buffik
+          </a>
+          {', '}
+        </p>
+        <p>Online Store © 2023</p>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
